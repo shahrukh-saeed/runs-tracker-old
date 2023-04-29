3009711add50203d761f899c322bd012d8c13229
shahrukh-saeed <117412909+shahrukh-saeed@users.noreply.github.com>
2023-04-29 02:35:37 -0400

    added login functionality

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+ACCOUNTS = [
+    {
+        'username': 'test1',
+        'password': '1',
+    },
+    {
+        'username': 'test2',
+        'password': '2'
+    }
+]
+
+GAMES = [
+    {
+        'username': 'test1',
+        'games': [
+            {
+                'id': 1,
+                'team1': 50,
+                'team2': 60
+            }
+        ]
+    },
+    {
+        'username': 'test2',
+        'games': [
+            {
+                'id': 2,
+                'team1': 35,
+                'team2': 30
+            }
+        ]
+    }
+]
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
+
+@app.route("/newaccount")
+def newAccount():
+    return render_template('newaccount.html')
+
+@app.route("/tracker", methods=['post'])
+def tracker():
+    data = request.form
+
+    for acc in ACCOUNTS:
+        if acc['username'] == data['username'] and acc['password'] == data['password']:
+            return render_template('/tracker.html')
+    
+    return "Wrong Username or Password"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
